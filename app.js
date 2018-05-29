e5c7fc429a162f04ae6f912f28bc62b3de3d5dcf
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-05-28 21:25:27 -0400

    Up to date May 28, 2018

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-const table = document.getElementById("pixelCanvas"); //retrieve table element
-const height = document.getElementById("inputHeight"); //retrieve Height
-const width = document.getElementById("inputWidth"); //retrieve Width
-const color = document.getElementById("colorPicker"); //retrieve current color
-const sizePicker = document.getElementById("sizePicker"); //retrieve form
+// Select color input
+const color = document.getElementById("colorPicker");
+// Select size input
+const width = document.getElementById("inputWidth");
+const height = document.getElementById("inputHeight");
+// Retrieve table element
+const table = document.getElementById("pixelCanvas");
 
+// When size is submitted by the user, call makeGrid()
+const sizePicker = document.getElementById("sizePicker");
+
+//      FUNCTIONS *********************************************
+function makeGrid(){
+  for( let n = 0; n < height.value; n++) {
+    const row = table.insertRow(n);
+    for( let m = 0; m < width.value; m++) {
+      const cell = row.insertCell(m);
+    }}}
+
+function clearGrid(){
+  while (table.firstChild){
+    table.removeChild(table.firstChild);
+  }}
+
+//    EVENT LISTENERS ****************************************
 sizePicker.addEventListener('submit', function(event){
   event.preventDefault();
   clearGrid();
   makeGrid();
-}
-
-function makeGrid() {
-for (let n = 0; n< height.value; n++) {   //while inputWidth is between 1 and value.
-      const row = table.insertRow(n);
-    for (let m = 0; m< width.value; m++) {  //while inputHeight is between 1 and value.
-      const cell = row.insertCell(m);
-}}}
-
-function clearGrid (){
-    while (table.firstChild){
-      table.removeChild(table.firstChild);
-}}
-
-  // Add Color.
-
-$(table).on("click", "td", function() {
-    $(this).css("background-color", $("#colorPicker").val());
 });
-    // Remove Color.
 
-$(table).on("dblclick", "td", function() {
-    $(this).removeAttr("style");
+$(table).on("click", "td", function(){
+  $(this).css("background-color", $("#colorPicker").val());
+});
+
+$(table).on("dblclick", "td", function(){
+  $(this).removeAttr("style");
 });
