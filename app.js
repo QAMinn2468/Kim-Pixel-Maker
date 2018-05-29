337d39227894afb1d1f3c1062f1f422f27ab55fa
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-05-28 21:16:59 -0400

    Updating file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var table = document.getElementById("pixelCanvas"); //retrieve table element
-var height = document.getElementById("inputHeightK"); //retrieve Height
-var width = document.getElementById("inputWidthK"); //retrieve Width
-var color = document.getElementById("colorPicker"); //retrieve current color
-var sizePicker = document.getElementById("sizePicker"); //retrieve form
+const table = document.getElementById("pixelCanvas"); //retrieve table element
+const height = document.getElementById("inputHeight"); //retrieve Height
+const width = document.getElementById("inputWidth"); //retrieve Width
+const color = document.getElementById("colorPicker"); //retrieve current color
+const sizePicker = document.getElementById("sizePicker"); //retrieve form
 
 sizePicker.onsubmit = function(event){
   event.preventDefault();
@@ -15,14 +15,12 @@ for (let n = 0; n< height.value; n++) {   //while inputWidth is between 1 and va
       const row = table.insertRow(n);
     for (let m = 0; m< width.value; m++) {  //while inputHeight is between 1 and value.
       const cell = row.insertCell(m);
-}
-  }
-}
+}}}
 
 function clearGrid (){
     while (table.firstChild){
       table.removeChild(table.firstChild);
-  }}
+}}
 
   // Add Color.
 
@@ -34,4 +32,3 @@ $(table).on("click", "td", function() {
 $(table).on("dblclick", "td", function() {
     $(this).removeAttr("style");
 });
-
